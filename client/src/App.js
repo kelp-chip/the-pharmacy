13aa2cbcce7ed7c6f347c508cd6c16e842556dca
Nathan Wong <natdanwongielhan@yahoo.com>
2021-04-09 17:44:34 -0700

    Created Rodal, need to add information to it

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
-import "./components/FauxLanding";
-import FauxLanding from "./components/FauxLanding";
+import "./components/FauxLanding.jsx";
+// import FauxLanding from "./components/FauxLanding";
+import "./components/AlcoholList.jsx";
+import AlcoholList from "./components/AlcoholList";
+
+let mockData = [
+    {
+      id: 1,
+      name: "gin",
+      price: 1.50,
+      description: "really great stuff my brother Al makes. Super strong too.",
+      images: "https://dydza6t6xitx6.cloudfront.net/ci-hendricks-gin-7270fa521eb29536.jpeg,"
+    },
+    {
+      id: 2,
+      name: "beer",
+      price: 2.00,
+      description: "tastes good",
+      images: "https://dydza6t6xitx6.cloudfront.net/ci-hendricks-gin-7270fa521eb29536.jpeg,"
+    },
+]
 
 function App() {
   return (
     <div>
-      <FauxLanding />
+      <AlcoholList drinks={mockData}/>
     </div>
   );
 }
---
+++ client/src/components/AlcoholList.jsx
@@ -0,0 +1,41 @@
+import "../AlcoholList.css";
+import React, {useState} from 'react';
+import Rodal from 'rodal';
+
+function AlcoholList (props) {
+
+  // const [name, ] = useState();
+  // const [price, ] = useState();
+  // const [description, ] = useState();
+  // const [images, ] = useState();
+  const [visibleRodal, toggleRodal] = useState(false);
+
+  function showRodal(){
+    toggleRodal(true)
+  }
+
+  function closeRodal(){
+    toggleRodal(false);
+  }
+
+  return (
+    <div>
+      {console.log(visibleRodal)}
+      {props.drinks.map( (drink, i) => {
+        return (
+          <div key ={i}>
+            <h3 className='drinkName' onClick={showRodal}>{(drink.name[0]).toUpperCase() + (drink.name).substring(1,drink.name.length)}</h3>
+            <div className='drinkPrice'>${drink.price.toFixed(2)}</div>
+            <div className='drinkDesc'>"{drink.description}"</div>
+            <img className='drinkImg' src={drink.images} alt='hi'></img>
+            <Rodal visible={visibleRodal} onClose={closeRodal}>
+              <div className='rodalName'> </div>
+            </Rodal>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default AlcoholList;
