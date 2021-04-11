a7672aef3a8119478b4ea9984b9227146512f35e
kyleperra <perra.kyle@gmail.com>
2021-04-11 05:00:26 -0700

    reconfigured data passing between store and order pages

--- client/src/pages/Order/components/BagItem.js
+++ client/src/pages/Order/components/BagItem.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import "../styles/BagItem.css";
+
 
 function BagItem({ drink }) {
   return (
-    <div>
-      <p>no :)</p>
-      <p>{drink.name}</p>
+    <div className="item-container">
+      <h3 className="drink-name">{drink.name}: </h3>
       <p>{drink.price}</p>
       <p>{drink.amount}</p>
       <button>Delete</button>
