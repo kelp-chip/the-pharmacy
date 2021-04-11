57d3a90a74a568cb2ba01753b37a5ba80bae9347
Lexy Kramer <lexy.kramer@gmail.com>
2021-04-11 04:03:35 -0400

    added store background and header

--- client/src/pages/Store/index.js
+++ client/src/pages/Store/index.js
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
+import "./styles/index.css";
+import left_bottles from "./styles/images/left_bottles.svg";
+import right_bottle from "./styles/images/right_bottle.svg";
+import banner from "./styles/images/banner.svg";
 
-import fakeData from '../../components/fakeData';
-import AlcoholList from './components/AlcoholList';
+import fakeData from "../../components/fakeData";
+import AlcoholList from "./components/AlcoholList";
 
-
-function Store ({ drinks }) {
+function Store({ drinks }) {
   return (
     <div>
-      <h1>The Store</h1>
+      <header>
+        <img src={banner} alt="banner"></img>
+      </header>
+      <div className="leftBottles">
+        <img src={left_bottles} alt="background bottles"></img>
+      </div>
+      <div className="rightBottles">
+        <img src={right_bottle} alt="background bottles"></img>
+      </div>
       <Link
         to={{
           pathname: "/order",
